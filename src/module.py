b574a2b0a4fc1712c8fe7661d24b30e4540f2dca
Kristian <34128194+khte@users.noreply.github.com>
2022-12-06 16:24:23 +0100

    Delete module.py

--- src/module.py
+++
@@ -1,9 +0,0 @@
-def add_str(*args, **kwargs):
-    """Function for adding strings. Input: strings separated by a space"""
-    kwargs_list = ['%s=%s' % (k, kwargs[k]) for k in kwargs]
-    print(''.join(args), ','.join(kwargs_list))
-
-def add_num(*args, **kwargs):
-    """Function for adding numbers. Input: numbers separated by a space. End with type=int/float"""
-    t = globals()['__builtins__'][kwargs['type']]
-    print(sum(map(t, args)))
